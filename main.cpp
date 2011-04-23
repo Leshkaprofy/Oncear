32b9a9f68f5c70ba4fadd4255897449ebd1d3060
Igor Sugak <once.mph@gmail.com>
2011-04-24 00:10:33 +0300

    some litle tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
 
+#include <QLabel>
+
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    QLabel *l = new QLabel("test");
+    l->show();
     return app.exec();
 }
